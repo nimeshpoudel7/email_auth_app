6eebae3c54360e78160679a47fe471c585748512
Nimesh Poudel <7.nimeshpoudel@gmail.com>
2021-07-01 17:49:14 +0545

    finsish

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@
 import './App.css';
 import Signin from './components/Signin';
 import Home from './components/Home';
+import { useEffect, useState } from 'react';
+import { auth } from './firebase';
 
 function App() {
+  const [user, setUsser] = useState(null)
+ 
+  useEffect(() => {
+    const unsubscribe= auth.onAuthStateChanged((userAuth)=>{
+      const user={
+        uid:userAuth?.uid,
+        email:userAuth?.email
+      }
+      if (userAuth) {
+        setUsser(user)
+    } else {
+      setUsser(null)
+    }
+  })
+  
+    return (unsubscribe)
+  }, [])
+  console.log('us',user)
   return (
     <div className="App">
-      <Home/>
-     <Signin/>
+          {user ? <Home /> : <Signin />}
+     
      
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { auth } from '../firebase'
 
 const Home = () => {
     return (
         <div>
         <h5>Welome to home</h5> 
+       <p><button onClick={() => auth.signOut()}>Sign out</button></p>
         </div>
     )
 }
